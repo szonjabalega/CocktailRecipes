14f244a712e04446050acd685c37c58849875442
szonjabalega <32750497+szonjabalega@users.noreply.github.com>
2023-05-21 17:15:57 +0200

    created layouts, updated fragments

--- app/src/main/java/com/szonjabalega/cocktailrecipes/persistence/CocktailsDatabase.kt
+++ app/src/main/java/com/szonjabalega/cocktailrecipes/persistence/CocktailsDatabase.kt
@@ -2,7 +2,7 @@ package com.szonjabalega.cocktailrecipes.persistence
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-@Database(entities = [Cocktail::class], version = 1)
+@Database(entities = [Cocktail::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun cocktailDao(): CocktailsDao
 }
--- app/src/main/java/com/szonjabalega/cocktailrecipes/ui/list/CocktailsListFragment.kt
+++ app/src/main/java/com/szonjabalega/cocktailrecipes/ui/list/CocktailsListFragment.kt
@@ -5,16 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.szonjabalega.cocktailrecipes.ui.details.DetailsViewModel
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.cocktailrecipes.databinding.FragmentCocktailsListBinding
+import com.szonjabalega.cocktailrecipes.model.Cocktails
 
 class CocktailsListFragment : Fragment() {
+    private var _binding: FragmentCocktailsListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var cocktailsAdapter: CocktailsListAdapter
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val cocktailsListViewModel = ViewModelProvider(this)[CocktailsListViewModel::class.java]
-        return super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentCocktailsListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val items = listOf(
+            Cocktails(1,"Margarita", "https://www.thecocktaildb.com/images/media/drink/6ty09d1504366461.jpg", "aaabbbb", "jcnsbkjcbsjbdc"),
+
+            Cocktails(2,"Margarita", "https://www.thecocktaildb.com/images/media/drink/6ty09d1504366461.jpg", "aaabbbb", "jcnsbkjcbsjbdc"),
+
+            Cocktails(3,"Margarita", "https://www.thecocktaildb.com/images/media/drink/6ty09d1504366461.jpg", "aaabbbb", "jcnsbkjcbsjbdc"),
+
+            Cocktails(4,"Margarita", "https://www.thecocktaildb.com/images/media/drink/6ty09d1504366461.jpg", "aaabbbb", "jcnsbkjcbsjbdc")
+            // Add more items as needed
+        )
+
+        cocktailsAdapter = CocktailsListAdapter(requireContext())
+        binding.recyclerView.adapter = cocktailsAdapter
+        binding.recyclerView.layoutManager = GridLayoutManager(requireContext(),2)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
